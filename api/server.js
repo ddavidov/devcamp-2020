531db05e61fcb4872b3093b99cf8bfe5b9fa54f9
ddavidov <ddavidov@mindk.com>
2020-12-21 11:23:45 +0200

    feat(posts): Add posts REST endpoints boilerplate

--- api/server.js
+++ api/server.js
@@ -1,10 +1,26 @@
-var express = require('express');
-var app = express();
+require('dotenv').config();
 
-app.get('/:name', function(req, res) {
-    res.send();
+const express = require('express');
+const app = express();
+
+// POSTS: (Create Read Update Delete)
+app.get('/posts', function(req, res) {
+    //@TODO: Posts list retrieve
+});
+app.get('/posts/:id', function(req, res) {
+    //@TODO: Get single post
+});
+app.post('/posts', function(req, res) {
+    //@TODO: Create new post
+});
+app.put('/posts/:id', function(req, res) {
+    //@TODO: Update existing post
+});
+app.delete('/posts/:id', function(req, res) {
+    //@TODO: Delete specified post
 });
 
-app.listen(3009, () => {
-    console.log('App server started!');
+
+app.listen(process.env.PORT, () => {
+    console.log(`App server started on ${process.env.PORT}!`);
 });
